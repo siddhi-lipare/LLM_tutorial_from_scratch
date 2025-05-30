e583896e400b4c18a815713c9970814668271ae6
siddhi-lipare <siddhi21@iiserb.ac.in>
2025-05-30 16:04:36 +0530

    tokeniser

--- chp2_tokeniser.py
+++ chp2_tokeniser.py
@@ -15,5 +15,19 @@ class SimpleTokeniser:
         text = " ".join([self.int_to_str[s] for s in ids])
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
-    
 
+
+with open("the-verdict.txt", "r", encoding = "utf-8") as f:
+    raw_text = f.read()
+
+result = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item for item in result if item.strip()]
+all_words = sorted(set(preprocessed))
+
+vocab = {token:integer for integer, token in enumerate(all_words)}
+
+tokenizer = SimpleTokeniser(vocab)
+text = """It's the last he painted, you know," Mrs. Gisburn said with pardonable pride. "The last but one," she corrected herself--"but the other doesn't count, because he destroyed it."""
+
+ids = tokenizer.encode(text)
+print(ids)
