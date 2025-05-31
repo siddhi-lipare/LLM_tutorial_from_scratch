c579ea646a8135cef681566b4a4c4a8e019a848a
siddhi-lipare <siddhi21@iiserb.ac.in>
2025-05-31 15:45:25 +0530

    reorganise

--- chp2/dataloader.py
+++ chp2/dataloader.py
@@ -40,11 +40,10 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+dataloader = create_dataloader_v1(raw_text, batch_size=4, max_length=4, stride=4, shuffle=False) # max_length is generally atleast 256  while training LLMs 
+#although smaller batch sizes take less memory, theey lead to more noisy model updates.
+
 data_iter = iter(dataloader)
 first_batch = next(data_iter)
 
 print(first_batch)
-
-
-
